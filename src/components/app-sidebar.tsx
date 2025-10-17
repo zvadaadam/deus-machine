5e9d0ae65d4e7deae2609d1982ea1298f902577a
Adam Zvada <zvadaada@gmail.com>
2025-10-18 00:40:37 +0200

    conductor-checkpoint-msg_014H9M3MNqxt4ursaVUPBkro

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -214,7 +214,7 @@ function RepositoryItem({
                     );
                   })()}
                   {hasRunningWorkspace && (
-                    <span className="absolute -bottom-0.5 -right-0.5 flex h-3 w-3">
+                    <span className="absolute -top-0.5 -right-0.5 flex h-3 w-3 z-10">
                       <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75"></span>
                       <span className="relative inline-flex rounded-full h-3 w-3 bg-blue-500"></span>
                     </span>
