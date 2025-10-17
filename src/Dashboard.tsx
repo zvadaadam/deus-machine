70bd6077de3f85c6202189ed1af8acf47db4bbf1
Adam Zvada <zvadaada@gmail.com>
2025-10-18 01:26:28 +0200

    Changes auto-committed by OpenDevs

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -314,7 +314,7 @@ export function Dashboard() {
         <div className="p-4 space-y-3">
           <Skeleton className="h-6 w-full" />
           <Skeleton className="h-4 w-4/5" />
-          <Skeleton className="h-4 w-9/10" />
+          <Skeleton className="h-4 w-[90%]" />
           <Skeleton className="h-4 w-3/4" />
         </div>
       ) : repoGroups.length === 0 ? (
