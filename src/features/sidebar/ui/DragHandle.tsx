20e4340d4e762b914ade2b18a5247256c0a2d67d
Adam Zvada <zvadaada@gmail.com>
2025-10-24 10:32:53 +0200

    conductor-checkpoint-msg_015Zfc15PNDrnk7LZpVD6dih

--- src/features/sidebar/ui/DragHandle.tsx
+++ src/features/sidebar/ui/DragHandle.tsx
@@ -24,7 +24,7 @@ export function DragHandle({
       {...listeners}
       className={cn(
         "absolute -left-2 top-1/2 -translate-y-1/2",
-        "opacity-0 group-hover:opacity-100",
+        "opacity-0 group-hover:opacity-100 group-data-[state=open]:opacity-100",
         "transition-opacity duration-200 ease-out",
         "text-sidebar-foreground/30 hover:text-sidebar-foreground/60",
         "cursor-grab active:cursor-grabbing",
